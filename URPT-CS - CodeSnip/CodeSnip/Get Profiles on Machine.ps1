afabdd4c09984cdfeb3f76914cb9c1012677e534
Code Ape <fardin.barashi@gmail.com>
2021-08-05 10:56:43 +0200

    Add files via upload

---
+++ URPT-CS - CodeSnip/CodeSnip/Get Profiles on Machine.ps1
@@ -0,0 +1,27 @@
+﻿# Start Logging
+Start-Transcript -Path "$PSScriptRoot\RemoveAccountLog.txt" -Force -Append
+Get-Date -Format "yyyy-mm-dd HH:MM"
+
+ # Get Profiles
+ Try
+  { # Start Try, Get Sid and translate them
+   $GetSids = Get-WmiObject -Class Win32_UserProfile -Filter "Special=False" | Select-Object Sid, @{Label='LastUseTime';Expression={$_.ConvertToDateTime($_.LastUseTime)}} 
+    ForEach ($UserSid in $GetSids)
+    { # Start ForEach ($UserSid in $GetSids)      
+     $SID = New-Object System.Security.Principal.SecurityIdentifier($UserSid.sid)            
+     $UserAccount = $SID.Translate([System.Security.Principal.NTAccount])            
+     $Profile = $UserAccount.value.split("\")[1];  
+
+     Write-Host $SID, $Profile, $UserSid.LastUseTime
+   } # End ForEach ($UserSid in $GetSids)     
+  } # End Try, Get Sid and translate them
+
+Catch
+  {  # Start Catch, Get Sid and translate them
+   Write-Warning -Message "## ERROR## " 
+   Write-Warning -Message "## Script could not start ## " 
+   Write-Warning $Error[0]
+  }  # End Catch, Get Sid and translate them
+
+# End Logging
+Stop-Transcript 
